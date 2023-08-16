ca140c93a3e86904b3bf8b7fbbf8f6ff3eb63354
Christian <christiansmith2394@gmail.com>
2023-08-15 21:49:56 -0400

    need to redo all folder/file layouts to add outing correctly

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar";
 
-export default function Home() {
+export default function MyApp() {
   return (
     <main className={styles.main}>
       <Navbar />
